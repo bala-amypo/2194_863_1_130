f015dd21fa1af8517915f7aff9cd3a1a6463e261
bala-amypo <bala@amypo.in>
2025-12-26 10:32:12 +0000

    User commit

--- src/main/java/com/example/demo/service/FraudAlertService.java
+++ src/main/java/com/example/demo/service/FraudAlertService.java
@@ -1,6 +1,13 @@
-// src/main/java/com/example/demo/service/FraudAlertService.java
 package com.example.demo.service;
 
 public interface FraudAlertService {
     void sendAlert(String alertMessage);
+}
+
+public interface FraudRuleService {
+    void applyRule(String ruleName);
+}
+
+public interface StolenDeviceService {
+    void reportDevice(String deviceId);
 }
--- src/main/java/com/example/demo/service/impl/FraudAlertServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/FraudAlertServiceImpl.java
@@ -1,12 +1,32 @@
-// src/main/java/com/example/demo/service/impl/FraudAlertServiceImpl.java
 package com.example.demo.service.impl;
 
 import com.example.demo.service.FraudAlertService;
 
 public class FraudAlertServiceImpl implements FraudAlertService {
-
     @Override
     public void sendAlert(String alertMessage) {
         System.out.println("Alert sent: " + alertMessage);
+    }
+}
+
+package com.example.demo.service.impl;
+
+import com.example.demo.service.FraudRuleService;
+
+public class FraudRuleServiceImpl implements FraudRuleService {
+    @Override
+    public void applyRule(String ruleName) {
+        System.out.println("Rule applied: " + ruleName);
+    }
+}
+
+package com.example.demo.service.impl;
+
+import com.example.demo.service.StolenDeviceService;
+
+public class StolenDeviceServiceImpl implements StolenDeviceService {
+    @Override
+    public void reportDevice(String deviceId) {
+        System.out.println("Device reported: " + deviceId);
     }
 }
