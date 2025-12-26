813f086cecbc8b796ef93fb59ffbaf16dc706aec
bala-amypo <bala@amypo.in>
2025-12-26 10:35:11 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/FraudAlertServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/FraudAlertServiceImpl.java
@@ -3,30 +3,14 @@ package com.example.demo.service.impl;
 import com.example.demo.service.FraudAlertService;
 
 public class FraudAlertServiceImpl implements FraudAlertService {
+
+    // Constructor
+    public FraudAlertServiceImpl() {
+        // Initialization if needed
+    }
+
     @Override
     public void sendAlert(String alertMessage) {
         System.out.println("Alert sent: " + alertMessage);
-    }
-}
-
-package com.example.demo.service.impl;
-
-import com.example.demo.service.FraudRuleService;
-
-public class FraudRuleServiceImpl implements FraudRuleService {
-    @Override
-    public void applyRule(String ruleName) {
-        System.out.println("Rule applied: " + ruleName);
-    }
-}
-
-package com.example.demo.service.impl;
-
-import com.example.demo.service.StolenDeviceService;
-
-public class StolenDeviceServiceImpl implements StolenDeviceService {
-    @Override
-    public void reportDevice(String deviceId) {
-        System.out.println("Device reported: " + deviceId);
     }
 }
