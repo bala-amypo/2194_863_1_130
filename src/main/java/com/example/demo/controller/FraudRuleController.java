a15a3588409384c5767769ccbbc87609a35731f3
bala-amypo <bala@amypo.in>
2025-12-28 18:31:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/FraudRuleController.java
+++ src/main/java/com/example/demo/controller/FraudRuleController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@PreAuthorize("hasRole('ADMIN')")
 @RequestMapping("/api/fraud-rules")
 public class FraudRuleController {
     
